88ea2858d90fc53018e8c75b2db261b37eba1942
wupeng <823105162@qq.com>
2019-05-30 17:03:11 +0800

    时间格式转换添加单元测试

--- BookReadDemoTests/BookReadDemoTests.m
+++ BookReadDemoTests/BookReadDemoTests.m
@@ -25,6 +25,39 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    NSString * formatTime = [self timeFormat:100000];
+    XCTAssertTrue([@"01:40" isEqualToString:formatTime],"成功");
+    
+    formatTime = [self timeFormat:59000];
+    XCTAssertTrue([@"00:59" isEqualToString:formatTime],"成功");
+    
+    formatTime = [self timeFormat:6000000];
+    XCTAssertTrue([@"100:00" isEqualToString:formatTime],"成功");
+    
+    formatTime = [self timeFormat:6010000];
+    XCTAssertTrue([@"100:10" isEqualToString:formatTime],"成功");
+}
+
+- (NSString *)timeFormat:(NSInteger)time{
+    NSInteger newTime = time/1000;
+    NSInteger minute = newTime/60;
+    NSInteger second = newTime%60;
+    
+    NSString * minuteString;
+    if (minute<10) {
+        minuteString = [NSString stringWithFormat:@"0%ld",minute];
+    }else{
+        minuteString = [NSString stringWithFormat:@"%ld",minute];
+    }
+
+    NSString * secondString;
+    if (second<10) {
+        secondString = [NSString stringWithFormat:@"0%ld",second];
+    }else{
+        secondString = [NSString stringWithFormat:@"%ld",second];
+    }
+    
+    return [NSString stringWithFormat:@"%@:%@",minuteString,secondString];
 }
 
 - (void)testPerformanceExample {
